6563486de4a0af8cfcb1cc6957cf099a04cf0a85
clong desktop <chip_long@yahoo.com>
2022-12-31 14:34:21 -0700

    added form validation

--- src/components/NewRestaurantFrom.js
+++ src/components/NewRestaurantFrom.js
@@ -1,18 +1,26 @@
 import { useState } from 'react';
 import { connect } from 'react-redux';
-import { Button, TextField } from '@mui/material';
+import { Alert, Button, TextField } from '@mui/material';
 import { createRestaurant } from '../store/restaurants/actions';
 
 export function NewRestaurantFrom({ createRestaurant }) {
   const [name, setName] = useState('');
+  const [validationError, setValidationError] = useState(false);
+
   async function handleSubmit(e) {
     e.preventDefault();
-    await createRestaurant(name);
+    if (name) {
+      setValidationError(false);
+      await createRestaurant(name);
+    } else {
+      setValidationError(true);
+    }
     setName('');
   }
 
   return (
     <form onSubmit={handleSubmit}>
+      {validationError && <Alert severity="error">Name is required</Alert>}
       <TextField
         placeholder="Add Restaurant"
         value={name}
--- src/components/NewRestaurantFrom.spec.js
+++ src/components/NewRestaurantFrom.spec.js
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { async } from 'q';
 import { NewRestaurantFrom } from './NewRestaurantFrom';
 
 describe('NewRestaurantForm', () => {
   const restaurantName = 'Sushi Place';
+  const requiredError = 'Name is required';
 
   let user;
   let createRestaurant;
@@ -15,6 +15,14 @@ describe('NewRestaurantForm', () => {
 
     render(<NewRestaurantFrom createRestaurant={createRestaurant} />);
   }
+
+  describe('initially', () => {
+    it('does not display a validation error', () => {
+      renderComponent();
+
+      expect(screen.queryByText(requiredError)).not.toBeInTheDocument();
+    });
+  });
 
   describe('when filled in', () => {
     async function fillInForm() {
@@ -35,6 +43,52 @@ describe('NewRestaurantForm', () => {
       await fillInForm();
 
       expect(screen.getByPlaceholderText('Add Restaurant')).toHaveValue('');
+    });
+
+    it('does not display a validation error', async () => {
+      await fillInForm();
+
+      expect(screen.queryByText(requiredError)).not.toBeInTheDocument();
+    });
+  });
+
+  describe('when empty', () => {
+    async function submitEmptyForm() {
+      renderComponent();
+
+      await user.click(screen.getByText('Add'));
+    }
+
+    it('displays a validation error', async () => {
+      await submitEmptyForm();
+
+      expect(screen.getByText(requiredError)).toBeInTheDocument();
+    });
+
+    it('does not call createRestaurant', async () => {
+      await submitEmptyForm();
+
+      expect(createRestaurant).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('when correcting a validation error', () => {
+    async function fixValidationError() {
+      renderComponent();
+      createRestaurant.mockResolvedValue();
+
+      await user.click(screen.getByText('Add'));
+      await user.type(
+        screen.getByPlaceholderText('Add Restaurant'),
+        restaurantName
+      );
+      await user.click(screen.getByText('Add'));
+    }
+
+    it('clears the validation error', async () => {
+      await fixValidationError();
+
+      expect(screen.queryByText(requiredError)).not.toBeInTheDocument();
     });
   });
 });
