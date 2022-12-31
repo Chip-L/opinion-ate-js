b0d6b333b75ddd7be870073ebd18c0843811a574
clong desktop <chip_long@yahoo.com>
2022-12-30 20:43:33 -0700

    setup submit functionality

--- src/components/NewRestaurantFrom.js
+++ src/components/NewRestaurantFrom.js
@@ -1,15 +1,24 @@
 import { Button, TextField } from '@mui/material';
-import React from 'react';
+import { useState } from 'react';
 
-export function NewRestaurantFrom() {
+export function NewRestaurantFrom({ createRestaurant }) {
+  const [name, setName] = useState('');
+  function handleSubmit(e) {
+    e.preventDefault();
+    createRestaurant(name);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <TextField
         placeholder="Add Restaurant"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
         fullWidth
         variant="filled"
       />
       <Button
+        type="submit"
         variant="contained"
         color="primary"
       >
---
+++ src/components/NewRestaurantFrom.spec.js
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import NewRestaurantFrom from './NewRestaurantFrom';
+
+describe('NewRestaurantForm', () => {
+  const restaurantName = 'Sushi Place';
+
+  let user;
+  let createRestaurant;
+
+  function renderComponent() {
+    user = userEvent.setup();
+    createRestaurant = jest.fn().mockName('createRestaurant');
+
+    render(<NewRestaurantFrom createRestaurant={createRestaurant} />);
+  }
+
+  describe('when filled in', () => {
+    async function fillInForm() {
+      renderComponent();
+      await user.type(
+        screen.getByPlaceholderText('Add Restaurant'),
+        restaurantName
+      );
+      await user.click(screen.getByText('Add'));
+    }
+
+    it('calls createRestaurant with the name', async () => {
+      await fillInForm();
+      expect(createRestaurant).toHaveBeenCalledWith(restaurantName);
+    });
+  });
+});
